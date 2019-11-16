8ae4e06eb5c3c70a68853726762cc9ee9b487707
Yiannis Stavrou <stav0049@gmail.com>
2019-11-15 20:45:25 -0600

    Update auth-guard.service.ts

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -7,7 +7,9 @@ import { AuthService } from './auth.service';
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(public auth: AuthService, public router: Router) { }
+  constructor(
+    public auth: AuthService, 
+    public router: Router) { }
 
   canActivate(): boolean {
     if (!this.auth.isAuthenticated()) {
